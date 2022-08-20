9ac9fcbe9804b5055a2b56775b191b4607260e76
andrew-m45 <106538699+andrew-m45@users.noreply.github.com>
2022-08-20 17:00:55 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Coin from './components/Coin'
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=false
 
 function App() {
   const [coins, setCoins] = useState([])
